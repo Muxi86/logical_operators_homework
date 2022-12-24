bc46bcbc412df04f8984a78c0e9b612cd5236f68
Muxiddin86 <imamberdievmuxiddin@gmail.com>
2022-12-24 18:26:55 +0500

    l19

--- logic19.py
+++ logic19.py
@@ -1,11 +1,11 @@
 def main(x):
     """
-    Given an integer x, return true if x is palindrome integer.
-    An integer is a palindrome when it reads the same backward as forward.
-
     Args:
         x(int): parameter x
     Returns:
         bool: answer
     """
-    return
+    return x % 10 == x // 100
+
+x = int(input())
+print(main(x))
