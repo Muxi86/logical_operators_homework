aba8bac61857ac9321606f2d7c942f11a3c1ea99
Muxiddin86 <imamberdievmuxiddin@gmail.com>
2022-12-24 22:12:40 +0500

    l19

--- logic19.py
+++ logic19.py
@@ -5,7 +5,7 @@ def main(x):
     Returns:
         bool: answer
     """
-    return x % 10 == x // 100
+    return (x >= 10 and x < pow(10,2) and x%10 == x//10) or (x >= pow(10,2) and x < pow(10,3) and x%10 == x//100)
 
 x = int(input())
 print(main(x))
